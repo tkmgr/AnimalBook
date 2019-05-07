b9f560d4ecf38a4b0d99f0f769f7893238f3ad32
tkmgr <taskmgrdev@gmail.com>
2019-05-07 20:50:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/animalbook/SubActivity.kt
@@ -0,0 +1,45 @@
+package com.example.animalbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sub.*
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        lionButton.setOnClickListener {
+            val fragment = LionFragment()
+            val fragmentManager = this.getSupportFragmentManager()
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        hippoButton.setOnClickListener {
+            val fragment = HippoFragment()
+            val fragmentManager = this.getSupportFragmentManager()
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        giraffeButton.setOnClickListener {
+            val fragment = GiraffeFragment()
+            val fragmentManager = this.getSupportFragmentManager()
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+
+        val fragment = titleFragment as? TitleFragment
+        fragment?.setTitle("図鑑画面")
+
+    }
+}
